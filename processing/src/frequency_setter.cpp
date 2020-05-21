19d293199dbf44fcf18a80092c133ee0abe3b9fe
Viktor Walter <viktor_walter@outlook.com>
2020-05-21 16:06:36 +0200

    Finished

--- processing/src/frequency_setter.cpp
+++ processing/src/frequency_setter.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <ros/package.h>
+#include <mrs_msgs/BacaProtocol.h>
 #include <uvdar/FrequencySet.h>
 
 namespace uvdar {
@@ -7,24 +8,43 @@ namespace uvdar {
     private: 
       bool initialized = false;
       ros::Subscriber frequency_subscriber;
+      ros::Publisher baca_protocol_publisher;
     public:
       FrequencySetter(ros::NodeHandle nh){
-      frequency_subscriber = nh.subscribe("freqnuencies_in" + std::to_string(i+1), 1, &UvdarKalman::measurementCallback, this);
+        frequency_subscriber = nh.subscribe("frequencies_in", 1, &FrequencySetter::frequencyCallback, this);
+        baca_protocol_publisher = nh.advertise<mrs_msgs::BacaProtocol>("baca_protocol_out", 1);
 
-
-      initialized = true;
+        initialized = true;
       }
 
       ~FrequencySetter(){
       }
 
 
+    private:
+      void frequencyCallback(const uvdar::FrequencySetConstPtr& msg){
+        mrs_msgs::BacaProtocol serial_msg;
+        serial_msg.stamp = ros::Time::now();
+        serial_msg.payload.push_back(0x92);
+
+        serial_msg.payload.push_back(msg->f1);
+        serial_msg.payload.push_back(msg->f2);
+        serial_msg.payload.push_back(msg->f3);
+        serial_msg.payload.push_back(msg->f4);
+        serial_msg.payload.push_back(msg->f5);
+        serial_msg.payload.push_back(msg->f6);
+        serial_msg.payload.push_back(msg->f7);
+        serial_msg.payload.push_back(msg->f8);
+
+        baca_protocol_publisher.publish(serial_msg);
+      }
+
   };
 }
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "uvdar_frequency_setter");
-  ros::NodeHandle nodeA;
+  ros::NodeHandle nodeA("~");
   uvdar::FrequencySetter        kl(nodeA);
 
   ROS_INFO("[UVDAR FrequencySetter]: frequency setter node initiated");
