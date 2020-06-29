307afeb011f051a61ae97c3a42e7d81b839e6171
Viktor Walter <viktor_walter@outlook.com>
2020-06-29 18:45:24 +0200

    Removed old scripts

--- scripts/record_rois.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/zsh
-rosbag record -a -x '/uav3/bluefox2/image_raw(.*)|/uav3/uvdar/reprojection/image_raw/compressedDepth(.*)' -O with_rois_visual.bag  
-
--- scripts/record_rois_new.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/zsh
-rosbag record -a -x '/uav42/bluefox2/image_raw(.*)|/uav42/reprojection/image_raw(.*)|/uav42/reprojection/image_raw/h264(.*)|/uav42/uvdar/reprojection/image_raw/compressedDepth(.*)|/uav42/uvdar/blink_visualization/image_raw/compressedDepth(.*)' -O with_rois_visual_new.bag  
-
