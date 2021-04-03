27adf1948b79e68f299e8f79af874749a806492a
Viktor Walter <viktor_walter@outlook.com>
2021-04-03 16:21:06 +0400

    Updating calibration script

--- scripts/record_calibration.sh
+++ scripts/record_calibration.sh
@@ -8,6 +8,9 @@ cleanup(){
 trap cleanup INT
 
 devnum=`rosrun bluefox2 bluefox2_list_cameras | tail -n1 | cut -d" " -f 3 | cut -d"," -f1`
-roslaunch bluefox2 single_nodelet.launch aec:=false agc:=false expose_us:=100 camera:=cam device:=${devnum} & \
-  rosbag record -a -x '(.*)compressed(.*)' -O ~/bag_files/uvdar_calib_tii/C_${devnum}.bag
+mkdir ~/bag_files
+mkdir ~/bag_files/uvdar_calib/
+mkdir ~/bag_files/uvdar_calib/${devnum}
+roslaunch bluefox2 single_nodelet.launch aec:=false agc:=false expose_us:=50 camera:=cam device:=${devnum} & \
+  rosbag record -a -x '(.*)compressed(.*)' -O ~/bag_files/uvdar_calib/${devnum}/C_${devnum}.bag
 
