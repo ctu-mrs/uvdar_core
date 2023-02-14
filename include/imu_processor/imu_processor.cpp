7d0b5fb78eb90e33767b3eebb8a3db88859744e8
Tim Lakemann <baitiaim@fel.cvut.cz>
2023-02-14 17:27:59 +0100

    added imu processor

---
+++ include/imu_processor/imu_processor.cpp
@@ -0,0 +1,16 @@
+#include "imu_processor.h"
+
+namespace uvdar{
+
+    IMU_PROCESSOR::IMU_PROCESSOR(/* args */)
+    {
+        std::cout << "HERE";
+
+    }
+    
+    IMU_PROCESSOR::~IMU_PROCESSOR()
+    {
+    }
+
+
+}
---
+++ include/imu_processor/imu_processor.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+#include <iostream>
+
+
+namespace uvdar{
+    class IMU_PROCESSOR
+    {
+    private:
+        /* data */
+    public:
+        IMU_PROCESSOR();
+        ~IMU_PROCESSOR();
+    };
+    
+    
+}
