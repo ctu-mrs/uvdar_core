ac95c2b630fcebf219b9117ab00bdae04ab70ef9
Tim Lakemann <baitiaim@fel.cvut.cz>
2023-04-07 13:19:38 +0200

    added config script

---
+++ config_tim_script.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd ~/git
+git clone https://github.com/caspososs/uvdar_core.git uvdar_core_tim
+cd uvdar_core_tim
+git checkout camp
+cd ~/
+mkdir -p ~/tim_l_ws/src && cd ~/tim_l_ws/src
+ln -s ~/git/uvdar_core_tim
+cd ~/tim_l_ws 
+catkin init
+catkin config --extend ~/modules_workspace/devel
+catkin build
+cd ~/tim_l_ws/src/uvdar_core_tim/
