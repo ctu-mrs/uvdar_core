1288c95730dc6d20f55ae866902905ea239ced91
ViktorWalter <viktor.walter@disroot.org>
2023-04-12 13:31:48 +0200

    Setting up termviz config

--- scripts/utilities/record_calibration.sh
+++ scripts/utilities/record_calibration.sh
@@ -3,6 +3,8 @@
 cleanup(){
   pkill -f rosbag
   pkill -f roslaunch
+  pkill -f termviz
+  pkill -f roscore
 }
 
 trap cleanup INT
@@ -11,6 +13,10 @@ devnum=`rosrun bluefox2 bluefox2_list_cameras | tail -n1 | cut -d" " -f 3 | cut 
 mkdir ~/bag_files
 mkdir ~/bag_files/uvdar_calib/
 mkdir ~/bag_files/uvdar_calib/${devnum}
-roslaunch bluefox2 single_nodelet.launch aec:=false agc:=false expose_us:=50 camera:=cam device:=${devnum} & \
-  rosbag record -a -x '(.*)compressed(.*)' -O ~/bag_files/uvdar_calib/${devnum}/C_${devnum}.bag
 
+roscore &
+
+sleep 2
+
+  roslaunch bluefox2 single_nodelet.launch aec:=false agc:=false expose_us:=50 camera:=cam device:=${devnum} & \
+  rosbag record -a -x '(.*)compressed(.*)' -O ~/bag_files/uvdar_calib/${devnum}/C_${devnum}.bag
