46777e7c73be57e9fd9977d4a65b095bb995fefc
Tim Lakemann <baitiaim@fel.cvut.cz>
2023-04-16 16:55:04 +0200

    lying star added

--- config/trajectory/build_trajectory.py
+++ config/trajectory/build_trajectory.py
@@ -65,6 +65,37 @@ def writeLine( name, start_point, end_point, line, number_data_pts, heading):
      
      file.close()
 
+def writeLine2( name, start_point, end_point, line, number_data_pts, heading):
+     file = open(name, 'a')
+     diff_y = end_point[1] - start_point[1]
+     diff_x = end_point[0] - start_point[0]
+     
+     discretization_x = float(diff_x) / float(number_data_pts) 
+     discretization =  float(diff_y) / float(number_data_pts)
+     discretization_angle = 0.71/ float(number_data_pts)
+     x_val = start_point[0]
+     # x_val = start_point[0] + discretization
+     y_val = start_point[1] # + discretization
+     for val in range(number_data_pts):
+          y_val = y_val + discretization
+          z = line[0] * y_val + line[1]
+          # z = 4.0
+          # y_val = 0.0
+          x_val = x_val + discretization_x
+          
+
+          if(x_val < 0.001 and x_val > -0.001):
+               start_point[0] = 0.0
+          if(z < 0.001 and z > -0.001):
+               z = 0.0
+          if(y_val < 0.001 and y_val > -0.001):
+               y_val = 0.0 
+          
+          # heading = heading + discretization_angle
+
+          file.write(str(x_val)[0:5]+" "+str(y_val)[0:5]+" "+str(z)[0:5]+" "+str(heading)[0:5]+"\n")
+     
+     file.close()
 
 def compute_star(name, start_point, second, third, fourth, fifth, heading, data_pts_per_line):
      line_1 = computeLine(start_point[1], start_point[2], second[1], second[2])
@@ -85,24 +116,51 @@ def compute_star(name, start_point, second, third, fourth, fifth, heading, data_
      writeLine(file_name, third, fourth, line_3, number_data_pts, 0.0)
      writeLine(file_name, fourth, fifth, line_4, number_data_pts, 0.0)
      writeLine(file_name, fifth, start_point, line_5, number_data_pts, 0.0)
+
+def compute_star2(name, start_point, second, third, fourth, fifth, heading, data_pts_per_line):
+     line_1 = computeLine(start_point[1], start_point[2], second[1], second[2])
+     line_2 = computeLine(second[1], second[2], third[1], third[2])
+     line_3 = computeLine(third[1], third[2], fourth[1], fourth[2])
+     line_4 = computeLine(fourth[1], fourth[2], fifth[1], fifth[2])
+     line_5 = computeLine(fifth[1], fifth[2], start_point[1], start_point[2])
+
+
+     file_name = name + "_" + str(start_point[0]) + "_" + str(start_point[1]) + "_" + str(start_point[2]) + ".txt"  
+     file = open(file_name, "w")
+     file.close()
+
+     number_data_pts = data_pts_per_line
+
+     writeLine2(file_name, start_point, second, line_1, number_data_pts, 0.0)
+     writeLine2(file_name, second, third, line_2, number_data_pts, 0.0)
+     writeLine2(file_name, third, fourth, line_3, number_data_pts, 0.0)
+     writeLine2(file_name, fourth, fifth, line_4, number_data_pts, 0.0)
+     writeLine2(file_name, fifth, start_point, line_5, number_data_pts, 0.0)
      
+
 
 def main():
      # writeCircle(name="trajectory_files/two_tx/tx2_circle", radius=1.5, x=8.0, y=-0.0, z=4.0, heading=0.0, data_points=60, ccw=1)
      # writeCircle(name="trajectory_files/two_tx/tx1_circle", radius=1, x=4.0, y=0.0, z=4.0, heading=0.0, data_points=100, ccw=1)
      
    
-     # name="trajectory_files/star"
-     # start_point = [8.0, 0.0, 6.0]
-     # left_down = [8.0, 2.0, 3.0]
-     # right_up = [8.0, -2.5, 5.5]
-     # left_up = [8.0, 2.5, 5.5]
-     # right_down = [8.0, -2.0, 3.0]
-     # compute_star(name, right_up, left_up, right_down, start_point, left_down, heading=0.0, data_pts_per_line=20) 
+     name="trajectory_files/star"
+     start_point = [6.0, 0.0, 6.0]
+     left_down = [4.0, 2.0, 3.0]
+     right_up = [6.0, -2.5, 5.5]
+     left_up = [6.0, 2.5, 5.5]
+     right_down = [4.0, -2.0, 3.0]
 
-     file = open("file.txt", "w")
-     l = computeLine(0,4,0,-4)
-     writeLine(name="file.txt", start_point=[0.0, 4.0, 4.0], end_point=[0.0, -4.0, 4.0], line=l, number_data_pts=120, heading=0.869)
+     # start_point = [4.0, 0.0, 6.0]
+     # left_down = [4.0, 2.0, 3.0]
+     # right_up = [4.0, -2.5, 5.5]
+     # left_up = [4.0, 2.5, 5.5]
+     # right_down = [4.0, -2.0, 3.0]
+     compute_star2(name, start_point, left_down, right_up, left_up, right_down, heading=0.0, data_pts_per_line=20) 
+
+     # file = open("file.txt", "w")
+     # l = computeLine(0,4,0,-4)
+     # writeLine(name="file.txt", start_point=[0.0, 4.0, 4.0], end_point=[0.0, -4.0, 4.0], line=l, number_data_pts=120, heading=0.869)
 
 if __name__ == "__main__":
     main()
