8954451b0f11f9080cc5a5eac9b7df2795a7e2d4
Tim Lakemann <baitiaim@fel.cvut.cz>
2023-10-05 11:51:27 +0200

    trajectories updated

--- config/trajectory/build_trajectory.py
+++ config/trajectory/build_trajectory.py
@@ -1,8 +1,11 @@
 import math
 
-def writeCircle(name, radius, x, y, z, heading, data_points, ccw):
-     file_name = name + "circle_" + str(x) + "_" + str(y) + "_" + str(z) + "_" + str(heading) + ".txt"  
-     file = open(file_name, 'w')
+def writeCircle(name, radius, center_point, heading, data_points, ccw):
+     
+     x = center_point[0]
+     y = center_point[1]
+     z = center_point[2]
+     file = open(name, 'w')
 
      angle_discretization = (2*math.pi) / float(data_points) * float(ccw)
      angle = 0
@@ -25,10 +28,12 @@ def writeCircle(name, radius, x, y, z, heading, data_points, ccw):
      file.close()
 
 
-def writeEight(name, radius, x, y, z, heading, data_points, ccw):
+def writeEight(name, radius, center_point, heading, data_points, ccw):
 
-     file_name = name + "eight_" + str(x) + "_" + str(y) + "_" + str(z) + "_" + str(heading) + ".txt"  
-     file = open(file_name, 'w')
+     x = center_point[0]
+     y = center_point[1]
+     z = center_point[2]
+     file = open(name, 'w')
 
      angle_discretization = (2*math.pi) / float(data_points) * float(ccw)
      angle = -math.pi / 2
@@ -74,32 +79,33 @@ def writeEight(name, radius, x, y, z, heading, data_points, ccw):
 
 
 
-def computeLine(first_x, first_y, second_x, second_y):
-    if(second_x - first_x == 0):
+def computeLine( start_point, end_point):
+    if(end_point[0] - start_point[0] == 0):
         slope = 0.0
     else:
-        slope = (second_y - first_y) / (second_x - first_x)
+        slope = (end_point[1] - start_point[1]) / (end_point[0] - start_point[0])
 
-    y_intercept = first_y - (slope * first_x)
+    y_intercept = start_point[1] - (slope * start_point[0])
     return slope,y_intercept
 
 
-def writeLine( name, start_point, end_point, line, number_data_pts, heading):
+def writeLine( name, start_point, end_point, line, number_data_pts, heading, x_rel_dist_change):
 
      file = open(name, 'a')
      
      diff_y = end_point[1] - start_point[1]
      discretization =  float(diff_y) / float(number_data_pts)
+     x_discretization = x_rel_dist_change / float(number_data_pts)
      discretization_angle = 0.0 #1.57/ float(number_data_pts)
      x_val = start_point[0]
-     # x_val = start_point[0] + discretization
+     x_val = start_point[0] + x_discretization
      y_val = start_point[1] + discretization
 
 
      for val in range(number_data_pts):
           y_val = y_val + discretization
           z = line[0] * y_val + line[1]
-          # x_val = x_val + discretization
+          x_val = x_val + x_discretization
           
 
           if(x_val < 0.001 and x_val > -0.001):
@@ -115,38 +121,6 @@ def writeLine( name, start_point, end_point, line, number_data_pts, heading):
      
      file.close()
 
-# def writeLine2( name, start_point, end_point, line, number_data_pts, heading):
-#      file = open(name, 'a')
-#      diff_y = end_point[1] - start_point[1]
-#      diff_x = end_point[0] - start_point[0]
-     
-#      discretization_x = float(diff_x) / float(number_data_pts) 
-#      discretization =  float(diff_y) / float(number_data_pts)
-#      discretization_angle = 0.71/ float(number_data_pts)
-#      # x_val = start_point[0]
-#      x_val = start_point[0] # + discretization_x
-#      y_val = start_point[1] # + discretization
-#      for val in range(number_data_pts):
-#           # y_val = y_val + discretization
-#           # z = line[0] * y_val + line[1]
-#           z = line[0] * x_val + line[1]
-#           # z = 4.0
-#           # y_val = 0.0
-#           x_val = x_val + discretization_x
-          
-
-#           if(x_val < 0.001 and x_val > -0.001):
-#                start_point[0] = 0.0
-#           if(z < 0.001 and z > -0.001):
-#                z = 0.0
-#           if(y_val < 0.001 and y_val > -0.001):
-#                y_val = 0.0 
-          
-#           # heading = heading + discretization_angle
-
-#           file.write(str(x_val)[0:5]+" "+str(y_val)[0:5]+" "+str(z)[0:5]+" "+str(heading)[0:5]+"\n")
-     
-#      file.close()
 
 def compute_star(name, start_point, second, third, fourth, fifth, heading, data_pts_per_line):
      line_1 = computeLine(start_point[1], start_point[2], second[1], second[2])
@@ -192,25 +166,50 @@ def compute_star2(name, start_point, second, third, fourth, fifth, heading, data
 
 def main():
 
+#### TX1 ####
+     tx1_directory = "trajectory_files/tx1/"
+# Line   
+     line_tx1_name = tx1_directory + "line.txt" 
+     file = open(line_tx1_name, 'w')
 
-     # writeCircle(name="trajectory_files/tx1/", radius=1.0, x=4.0, y=0.0, z=4.0, heading=0.0, data_points=60, ccw=1)
-     # writeCircle(name="trajectory_files/tx2/", radius=1.6, x=8.0, y=0.0, z=4.0, heading=0.0, data_points=60, ccw=1)
-     
-     # for height change 
-     # writeLine(name="trajectory_files/tx1/line", start_point=[4.0, 4.0, 4.0], end_point=[4.0, -4.0, 6.0], line=computeLine(4.0, 4.0, -4.0, 6.0), number_data_pts=30, heading=0.0)
-     
-     name_tx1 = "trajectory_files/tx1/line.txt" 
-     name_tx2 = "trajectory_files/tx2/line.txt" 
-     file = open(name_tx1, 'w')
-     file = open(name_tx2, 'w')
+     tx1_start = [4.0, 4.0, 4.0]
+     tx1_end =   [5.0, -4.0, 6.0]
 
-     writeLine(name=name_tx1, start_point=[4.0, 4.0, 4.0], end_point=[4.0, -4.0, 4.0], line=computeLine(4.0, 4.0, 4.0, -4.0), number_data_pts=30, heading=0.0)
-     writeLine(name=name_tx1, start_point=[4.0, -4.0, 4.0], end_point=[4.0, 4.0, 4.0], line=computeLine(4.0, 4.0, 4.0, -4.0), number_data_pts=30, heading=0.0)
-     # writeLine(name=name_tx2, start_point=[8.0, 4.0, 4.0], end_point=[8.0, -4.0, 4.0], line=computeLine(4.0, 4.0, 4.0, -4.0), number_data_pts=30, heading=0.0)
+     tx1_line = computeLine(start_point=[tx1_start[1], tx1_start[2]], end_point=[tx1_end[1], tx1_end[2]] )
 
-     # writeEight(name="trajectory_files/tx1/tx1", radius=1.0, x=4.0, y=0.0, z=4.0, heading=0.0, data_points=50, ccw=1)
+     writeLine(name=line_tx1_name, start_point=tx1_start, end_point=tx1_end, line=tx1_line, number_data_pts=30, heading=0.0, x_rel_dist_change= 1.0)
+     writeLine(name=line_tx1_name, start_point=tx1_end, end_point=tx1_start, line=tx1_line, number_data_pts=30, heading=0.0, x_rel_dist_change= -1.0)
+## center point for circle motions 
+     tx1_center_point = [4.0, 0.0, 4.0]
+#### Circle 
+     writeCircle(name= tx1_directory + "circle.txt" , radius=1.3, center_point=tx1_center_point, heading=0.0, data_points=60, ccw=1)
+#### Eight
+     writeEight(name= tx1_directory  + "eight.txt", radius=1.3, center_point=tx1_center_point, heading=0.0, data_points=50, ccw=1)
+
+
+# TX2 #
+     tx2_directory = "trajectory_files/tx2/"
+#### Line   
+     line_tx2_name = tx2_directory + "line.txt" 
+     file = open(line_tx2_name, 'w')
+
+     tx2_start = [8.0, 4.0, 4.0]
+     tx2_end =   [9.0, -4.0, 6.0]
+
+     tx2_line = computeLine(start_point=[tx2_start[1], tx2_start[2]], end_point=[tx2_end[1], tx2_end[2]] )
+
+     writeLine(name=line_tx2_name, start_point=tx2_start, end_point=tx2_end, line=tx2_line, number_data_pts=30, heading=0.0, x_rel_dist_change=1.0)
+     writeLine(name=line_tx2_name, start_point=tx2_end, end_point=tx2_start, line=tx2_line, number_data_pts=30, heading=0.0, x_rel_dist_change=-1.0)
+
+## center point for circle motions 
+     tx2_center_point = [8.0, 0.0, 4.0]
+#### Circle 
+     writeCircle(name= tx2_directory + "circle.txt" , radius=1.6, center_point=tx2_center_point, heading=0.0, data_points=60, ccw=1)
+#### Eight
+     writeEight(name= tx2_directory + "eight.txt", radius=1.6, center_point=tx2_center_point, heading=0.0, data_points=50, ccw=1)
+
+
      # writeEight(name="trajectory_files/tx2/tx2", radius=1.3, x=8.0, y=0.0, z=4.0, heading=0.0, data_points=50, ccw=-1)
-
      # name="trajectory_files/"
      # start_point = [-29.0, -22.0, 6.0]
      # left_down = [-29.0, -20.0, 3.0]
