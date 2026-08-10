6297cad1f5f37d7af7c20df49fe7af92039ed63e
TimLakemann <tim.lakemann@gmail.com>
2026-08-10 17:09:15 +0200

    use sim time flag

--- launch/filter.launch.py
+++ launch/filter.launch.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import EnvironmentVariable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
@@ -8,6 +8,7 @@ from launch_ros.substitutions import FindPackageShare
 def generate_launch_description():
     config_file = LaunchConfiguration("config_file")
     namespace = LaunchConfiguration("namespace")
+    use_sim_time = LaunchConfiguration("use_sim_time")
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -19,12 +20,16 @@ def generate_launch_description():
             ]),
         ),
         DeclareLaunchArgument("namespace", default_value=""),
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value=EnvironmentVariable("USE_SIM_TIME", default_value="false"),
+        ),
         Node(
             package="uvdar_core",
             executable="filter_node",
             name="filter",
             namespace=namespace,
             output="screen",
-            parameters=[{"config_path": config_file}],
+            parameters=[{"config_path": config_file, "use_sim_time": use_sim_time}],
         ),
     ])
